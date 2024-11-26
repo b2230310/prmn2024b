3eab8136f09bd441ab6f9921bf2a8c0f85ab9900
b2230310 <b2230310@photon.chitose.ac.jp>
2024-11-26 14:01:43 +0900

    prac1 gakusekiを割ä½作成した

---
+++ prmn2024Fall/src/main/java/lecture2/Gakuseki.java
@@ -0,0 +1,16 @@
+package lecture2;
+
+public class Gakuseki {
+
+    void Hantei(String number,String gakusekinumber){
+
+        if(number.equals(gakusekinumber)){
+            System.out.println( "complete!!" );
+        }else{
+            System.out.println( "error!!" );
+        }
+    }
+
+
+}
+
